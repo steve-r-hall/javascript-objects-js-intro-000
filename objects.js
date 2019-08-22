dc6e5c3abee6f6f7be2ec6a54a45af82b1a3ff5e
Stephen Robert hall <steve.r.hall@outlook.com>
2019-08-22 08:55:42 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = { fleetwoodmac: "albatross" };
+
+function updatePlaylist(obj, artist, title) {
+  Object.assign(obj, {[artist]: title});
+  return obj
+}
+
+function removeFromPlaylist (obj, artist) {
+    delete obj.artist;
+    return obj
+}
+
